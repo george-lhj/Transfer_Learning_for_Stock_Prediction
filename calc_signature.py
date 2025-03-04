03416ee360a1ab912592159b5c897debd59c3dad
George Liu <gliu@Georges-MacBook-Pro.local>
2025-03-03 21:41:00 -0800

    updated alans codes

--- calc_signature.py
+++ calc_signature.py
@@ -1,14 +1,84 @@
 import numpy as np
 import pandas as pd
 import iisignature
+from tqdm import tqdm
+from joblib import Parallel, delayed
 
-def calc_signature(x: pd.DataFrame, y: pd.DataFrame, order=3):
+def noise_reduction_variables_by_sector(df: pd.DataFrame):
+    # Feature engineering
+    df = df.copy()
+    new_features = []
+
+    # Conditional aggregated features
+    shifts = [1,5]  # 选择不同的滞后期
+    statistics = ['skew']  # 增加鲁棒性统计量
+    gb_features = ['SECTOR', 'DATE']
+    target_feature = ['RET', 'VOLUME']
+    tmp_name = '_'.join(gb_features)
+
+    # 计算不同滞后期的特征
+    for shift in shifts:
+        for stat in statistics:
+            for target in target_feature:
+                name = f'{shift}_{tmp_name}_{stat}_{target}'
+                feat = f'{target}_{shift}'
+                new_features.append(name)
+                if stat == 'median':
+                    df[name] = df.groupby(gb_features)[feat].transform('median')
+                elif stat == 'skew':
+                    df[name] = df.groupby(gb_features)[feat].transform(lambda x: x.skew())
+                elif stat == 'kurtosis':
+                    df[name] = df.groupby(gb_features)[feat].transform(lambda x: x.kurtosis())
+    return new_features, df
+
+def process_group(key_group, sig_col, order):
+    (stock, id_, industry, industry_group, sector, sub_industry), group = key_group
+    group = group.sort_values("DAY", ascending=True)
+    
+    # Compute additional indicators:
+    group['VOLATILITY_RET'] = group['RET'].std()
+    group['VOLATILITY_VOLUME'] = group['VOLUME'].std()
+    group['skew_20RET'] = group['RET'].skew()
+    group['kurtosis_20VOLUME'] = group['VOLUME'].kurtosis()
+
+    sig_col = sig_col + ['VOLATILITY_RET', 'VOLATILITY_VOLUME', 'skew_20RET', 'kurtosis_20VOLUME']
+    path = group[sig_col].values.astype(np.float64)
+    base_sig = iisignature.sig(path, order)
+    sig = np.insert(base_sig, 0, 1.0)  # augmented signature
+
+    # Optionally, trigger garbage collection if needed:
+    # gc.collect()
+    return sig, (stock, id_, industry, industry_group, sector, sub_industry)
+
+def calculation_signature_using_multiprocessing(df, order=3, sig_col=[]):
+    grouped = list(df.groupby(["STOCK", "ID", "INDUSTRY", "INDUSTRY_GROUP", "SECTOR", "SUB_INDUSTRY"]))
+    print(len(grouped))
+    total_groups = len(grouped)
+
+    results = Parallel(n_jobs=-1, backend='loky')(
+        delayed(process_group)(key_group, sig_col, order)
+        for key_group in tqdm(grouped, total=total_groups, desc="Processing groups")
+    )
+    signatures, keys = zip(*results)
+    sig_length = iisignature.siglength(len(sig_col), order)
+    sig_columns = [f"SIG_{i}" for i in range(sig_length+1)]
+    # Pre-allocate NumPy arrays for keys and signatures:
+    keys_np = np.array(keys)  # shape: (total_groups, 6)
+    signatures_np = np.array(signatures, dtype=np.float64)  # shape: (total_groups, len(sig_columns))
+    # Concatenate keys and signatures along the column axis
+    combined_np = np.hstack([keys_np, signatures_np])
+    columns = ["STOCK", "ID", "INDUSTRY", "INDUSTRY_GROUP", "SECTOR", "SUB_INDUSTRY"] + sig_columns
+    # df_signature = pd.DataFrame(combined_np, columns=columns)
+    return columns, combined_np
+
+def prepare_df_for_signature_computation(x: pd.DataFrame, y: pd.DataFrame, save=False, filename="Data"):
     df = x.sort_values(by=['STOCK', "ID"]).reset_index(drop=True)
-    df = df.drop(columns=['DATE'])
     df = df.fillna(0)
-
     ret_cols = [col for col in df.columns if col.startswith('RET')]
     df[ret_cols] = df[ret_cols].apply(lambda x: np.log1p(x))
+
+    # noise reduction
+    new_features, new_features_df = noise_reduction_variables_by_sector(df)
 
     ret_cols = sorted(
         [col for col in df.columns if col.startswith("RET_")],
@@ -23,24 +93,29 @@ def calc_signature(x: pd.DataFrame, y: pd.DataFrame, order=3):
     rets = df[ret_cols].values.ravel()
     volumes = df[volume_cols].values.ravel()
 
+    # feature_values = {}
+    # for feature in new_features:
+    #     feature_values[feature] = np.repeat(df[feature].values.ravel(), 20)
+
     # repeat INDUSTRY, INDUSTRY_GROUP, SECTOR, SUB_INDUSTRY for each RET and VOLUME
-    industry = np.repeat(df["INDUSTRY"].values, len(ret_cols))
-    industry_group = np.repeat(df["INDUSTRY_GROUP"].values, len(ret_cols))
-    sector = np.repeat(df["SECTOR"].values, len(ret_cols))
-    sub_industry = np.repeat(df["SUB_INDUSTRY"].values, len(ret_cols))
+    industry = np.repeat(df["INDUSTRY"].values, 20)
+    industry_group = np.repeat(df["INDUSTRY_GROUP"].values, 20)
+    sector = np.repeat(df["SECTOR"].values, 20)
+    sub_industry = np.repeat(df["SUB_INDUSTRY"].values, 20)
 
     # generate day for RET{i} and VOLUME{i}
     days = np.tile(np.arange(20,0,-1), len(df))
 
-
     # generate ID and stock (repeat 20 times)
-    ids = np.repeat(df["ID"].values, len(ret_cols))
-    stocks = np.repeat(df["STOCK"].values, len(ret_cols))
+    ids = np.repeat(df["ID"].values, 20)
+    stocks = np.repeat(df["STOCK"].values, 20)
+    dates = np.repeat(df["DATE"].values, 20)
 
     df_signature = pd.DataFrame({
         "ID": ids,
         "STOCK": stocks,
         "DAY": days,
+        "DATE": dates,
         "RET": rets,
         "VOLUME": volumes,
         "INDUSTRY": industry,
@@ -48,6 +123,9 @@ def calc_signature(x: pd.DataFrame, y: pd.DataFrame, order=3):
         "SECTOR": sector,
         "SUB_INDUSTRY": sub_industry
     })
+
+    # for feature in new_features:
+    #     df_signature[feature] = feature_values[feature]
 
     df_signature = df_signature.sort_values(["STOCK", "ID", "DAY"])
 
@@ -57,32 +135,38 @@ def calc_signature(x: pd.DataFrame, y: pd.DataFrame, order=3):
         (df_signature['VOLUME'] >= df_signature['VOLUME'].quantile(0.005)) & 
         (df_signature['VOLUME'] <= df_signature['VOLUME'].quantile(0.995))
     ]
+    if save:
+        df_signature.to_parquet(f"./datasets/{filename}.parquet", engine='pyarrow', compression='snappy')  # 保存为 Parquet
+        new_features_df.to_parquet(f"./datasets/{filename}_new_features.parquet", engine='pyarrow', compression='snappy')  # 保存为 Parquet
+        with open(f"./datasets/{filename}_new_features.txt", "w") as f:
+            f.write(str(new_features))
+    return new_features, new_features_df, df_signature
+    # return df_signature
 
-    sig_col = ["RET", "VOLUME", "DAY"]
-    grouped = df_signature.groupby(["STOCK", "ID", 'INDUSTRY', 'INDUSTRY_GROUP', 'SECTOR', 'SUB_INDUSTRY'])
-    signatures = []
-    keys = []
-
-    for (stock, id_, industry, industry_group, sector, sub_industry), group in grouped:
-        group = group.sort_values("DAY", ascending=True)
-        path = group[sig_col].values.astype(np.float64)
-
-        base_sig = iisignature.sig(path, order)
-        sig = np.insert(base_sig, 0, 1.0) # augmented signature
-        signatures.append(sig)
-        keys.append((stock, id_, industry, industry_group, sector, sub_industry))
+def calc_signature(df, order, new_features, filename, save=False):
+    df = df.copy().fillna(0)
+    # Define the list of columns for signature computation:
+    sig_lst = [
+        "RET",          # Original log-transformed return
+        "VOLUME",       # Trading volume
+        "DAY",          # Time indicator
+        "VOLATILITY_RET",   # standard deviation of RET of that stock in 20 days
+        "VOLATILITY_VOLUME",   # standard deviation of VOLUME of that stock in 20 days
+    ]
     
-    sig_length = iisignature.siglength(len(sig_col), order)
-    sig_columns = [f"SIG_{i}" for i in range(sig_length+1)]
-    
-    df_signature = pd.DataFrame(signatures, columns=sig_columns)
-    df_signature[["STOCK", "ID", 'INDUSTRY', 'INDUSTRY_GROUP', 'SECTOR', 'SUB_INDUSTRY']] = keys
-    
-    # Reorganize columns
-    cols_order = ["STOCK", "ID", 'INDUSTRY', 'INDUSTRY_GROUP', 'SECTOR', 'SUB_INDUSTRY'] + sig_columns
-    df_final = df_signature[cols_order]
+    # Assume df_signature is your input DataFrame
+    column, combined_np_array = calculation_signature_using_multiprocessing(df, order=order, sig_col=sig_lst)
+    column_array = np.array(column, dtype='U')
+    if save == True:
+        np.savez_compressed(
+            f"./datasets/{filename}_signature_data.npz",
+            features=column_array,          # 列名单独存储
+            data=combined_np_array.astype(np.float64)
+        )
+        print(f"Signature data saved to ./datasets/{filename}_signature_data.npz")
 
-    df_final = pd.merge(df_final, y, on=["ID"], how="inner")
-    df_final['RET'] = df_final["RET"].astype(int)
+    # # df_final = pd.DataFrame(combined_np_array, columns=column)
+    # df_final = pd.merge(df_final, y, on=["ID"], how="inner")
+    # df_final['RET'] = df_final["RET"].astype(int)
 
-    return df_final
+    # return df_final
--- ridge_regression_by_sector.py
+++ ridge_regression_by_sector.py
@@ -2,92 +2,125 @@ import numpy as np
 import pandas as pd
 import iisignature
 from iisignature import sig, prepare, logsig, logsiglength
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
 from sklearn.linear_model import RidgeClassifier, LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import StandardScaler
 from calc_signature import *
+from tqdm import tqdm
 
 
-x_train = pd.read_csv("/Users/runminghuang/Desktop/capstone/Data/x_train.csv")
-y_train = pd.read_csv("/Users/runminghuang/Desktop/capstone/Data/y_train.csv")
-x_test = pd.read_csv("/Users/runminghuang/Desktop/capstone/Data/x_test.csv")
-y_test = pd.read_csv("/Users/runminghuang/Desktop/capstone/Data/test_rand.csv")
+if __name__ == "__main__":
+    x_train = pd.read_csv("./datasets/x_train.csv")
+    y_train = pd.read_csv("./datasets/y_train.csv")
+    x_test = pd.read_csv("./datasets/x_test.csv")
+    y_test = pd.read_csv("./datasets/test_rand.csv")
 
-# df_train = calc_signature(x_train, y_train, order=3)
-# df_test = calc_signature(x_test, y_test, order=3)
+    # # Generate df_train and df_test and save them as parquet files
+    new_features_train, df_train_new_features, df_train_sig_prepared = prepare_df_for_signature_computation(x_train, y_train, save=True, filename="df_train")
+    # new_features_test, df_test_new_features, df_test_sig_prepared = prepare_df_for_signature_computation(x_test, y_test, save=True, filename="df_test")
 
-# df_train.to_csv("/Users/runminghuang/Desktop/capstone/Data/df_train.csv", index=False)
-# df_test.to_csv("/Users/runminghuang/Desktop/capstone/Data/df_test.csv", index=False)
+    # # Calculate signature for df_train and df_test and save them as npz files
+    df_train_sig_prepared = pd.read_parquet("./datasets/df_train.parquet")
+    # df_test_sig_prepared = pd.read_parquet("./datasets/df_test.parquet")
+    with open("./datasets/df_train_new_features.txt", "r") as f:
+        new_features = eval(f.read())
 
-df_train = pd.read_csv("/Users/runminghuang/Desktop/capstone/Data/df_train.csv")
-df_test = pd.read_csv("/Users/runminghuang/Desktop/capstone/Data/df_test.csv")
+    calc_signature(df_train_sig_prepared, order=3, new_features=new_features, filename="df_train", save=True)
+    # calc_signature(df_test_sig_prepared, order=3, new_features=new_features, filename="df_test", save=True)
 
-industry_lst = df_train.INDUSTRY.unique() # 'INDUSTRY'
-industry_group_lst = df_train.INDUSTRY_GROUP.unique() # 'INDUSTRY_GROUP'
-sector_lst = df_train.SECTOR.unique() # 'SECTOR'
-sub_industry_lst = df_train.SUB_INDUSTRY.unique() # 'SUB_INDUSTRY'
+    # # RIDGE REGRESSION BY SECTOR
+    loaded_train = np.load("./datasets/df_train_signature_data.npz")
+    # loaded_test = np.load("./datasets/df_test_signature_data.npz")
 
-param_grid = {"alpha": np.logspace(-3, 3, num=10)}
-results = []
+    df_train_final = pd.DataFrame(data=loaded_train['data'], columns=loaded_train['features'])
+    # df_test_final = pd.DataFrame(data=loaded_test['data'], columns=loaded_test['features'])
 
-for sector_idx in sector_lst:
-    try:
-        # Clean memory
-        import gc
-        gc.collect()
-        df_sector_train = df_train[df_train["SECTOR"] == sector_idx]
-        df_sector_test = df_test[df_test["SECTOR"] == sector_idx]
-        # Build feature matrix
-        feature_cols = [c for c in df_sector_train.columns if c.startswith("SIG_")]
-        
-        X_train = df_sector_train[feature_cols].values
-        y_train = np.array(df_sector_train["RET"])
-        X_test = df_sector_test[feature_cols].values
-        y_test = np.array(df_sector_test["RET"])
-        
-        # Scale the features
-        scaler = StandardScaler()
-        X_train_scaled = scaler.fit_transform(X_train)  
-        X_test_scaled = scaler.transform(X_test)
+    df_train_new_features = pd.read_parquet("./datasets/df_train_new_features.parquet")
+    # df_test_new_features = pd.read_parquet("./datasets/df_test_new_features.parquet")
 
-        # Train the model on the training set
-        # Grid search (5-fold CV)
-        gs = GridSearchCV(
-            estimator=RidgeClassifier(),
-            param_grid=param_grid,
-            cv=5,
-            scoring="accuracy",
-            # scoring="f1",
-            n_jobs=1
-        )
-        gs.fit(X_train_scaled, y_train)
+    y_dict_train_RET = y_train.set_index('ID')['RET'].to_dict()
+    # y_dict_test_RET = y_test.set_index('ID')['RET'].to_dict()
+    y_dict_train_new_features = df_train_new_features.set_index('ID')[new_features].to_dict()
+    # y_dict_test_new_features = df_test_new_features.set_index('ID')[new_features].to_dict()
 
-        # Evaluate on validation set
-        best_alpha = gs.best_params_["alpha"]
-        best_model = gs.best_estimator_
-        y_val_pred = best_model.predict(X_test_scaled)
-        acc = accuracy_score(y_test, y_val_pred)
-        report = classification_report(y_test, y_val_pred)
+    df_train_final['RET'] = df_train_final['ID'].map(y_dict_train_RET)
+    # df_test_final['RET'] = df_test_final['ID'].map(y_dict_test_RET)
+    df_train_final[new_features] = df_train_final['ID'].map(y_dict_train_new_features)
+    # df_test_final[new_features] = df_test_final['ID'].map(y_dict_test_new_features)
+    print(111)
 
-        # Store results
-        results.append({
-            "Grouped_by": sector_idx,
-            "Train_Samples": len(df_sector_train),
-            "Val_Samples": len(df_sector_test),
-            "Best_Alpha": best_alpha,
-            "Val_Accuracy": acc,
-            "Report": report,
-            "Model": best_model
-        })
 
-    except Exception as e:
-        print(f"Error processing sector {sector_idx}: {str(e)}")
-        continue
 
-# Summarize
-df_results = pd.DataFrame(results)
-print(df_results[[
-    "Grouped_by", "Train_Samples", "Val_Samples",
-    "Best_Alpha", "Val_Accuracy"
-]])
+
+    # industry_lst = df_train_final.INDUSTRY.unique() # 'INDUSTRY'
+    # industry_group_lst = df_train_final.INDUSTRY_GROUP.unique() # 'INDUSTRY_GROUP'
+    # sector_lst = df_train_final.SECTOR.unique() # 'SECTOR'
+    # sub_industry_lst = df_train_final.SUB_INDUSTRY.unique() # 'SUB_INDUSTRY'
+
+    # param_grid = {"alpha": np.logspace(-3, 3, num=10)}
+    # results = []
+
+    # for sector_idx in tqdm(sector_lst, total=len(sector_lst), desc="Processing training and prediction"):
+    #     try:
+    #         # Clean memory
+    #         import gc
+    #         gc.collect()
+    #         df_sector_train = df_train_final[df_train_final["SECTOR"] == sector_idx]
+    #         df_sector_test = df_test_final[df_test_final["SECTOR"] == sector_idx]
+    #         # Build feature matrix
+    #         feature_cols = [c for c in df_sector_train.columns if c.startswith("SIG_")]
+            
+    #         print(1)
+    #         X_train = df_sector_train[feature_cols].values
+    #         y_train = np.array(df_sector_train["RET"])
+    #         X_test = df_sector_test[feature_cols].values
+    #         y_test = np.array(df_sector_test["RET"])
+    #         print(2)
+    #         # Scale the features
+    #         scaler = StandardScaler()
+    #         X_train_scaled = scaler.fit_transform(X_train)  
+    #         X_test_scaled = scaler.transform(X_test)
+    #         print(3)
+    #         # Train the model on the training set
+    #         # Grid search (3-fold CV)
+    #         gs = GridSearchCV(
+    #             estimator=RidgeClassifier(solver="svg"),
+    #             param_grid=param_grid,
+    #             cv=3,
+    #             scoring="accuracy",
+    #             # scoring="f1",
+    #             n_jobs=-1
+    #         )
+    #         gs.fit(X_train_scaled, y_train)
+
+    #         print(4)
+    #         # Evaluate on validation set
+    #         best_alpha = gs.best_params_["alpha"]
+    #         best_model = gs.best_estimator_
+    #         y_val_pred = best_model.predict(X_test_scaled)
+    #         acc = accuracy_score(y_test, y_val_pred)
+    #         report = classification_report(y_test, y_val_pred)
+    #         print(5)
+    #         # Store results
+    #         results.append({
+    #             "Grouped_by": sector_idx,
+    #             "Train_Samples": len(df_sector_train),
+    #             "Val_Samples": len(df_sector_test),
+    #             "Best_Alpha": best_alpha,
+    #             "Val_Accuracy": acc,
+    #             "Report": report,
+    #             "Model": best_model
+    #         })
+
+    #     except Exception as e:
+    #         print(f"Error processing sector {sector_idx}: {str(e)}")
+    #         continue
+
+    # # Summarize
+    # df_results = pd.DataFrame(results)
+    # print(df_results[[
+    #     "Grouped_by", "Train_Samples", "Val_Samples",
+    #     "Best_Alpha", "Val_Accuracy"
+    # ]])
+    # # df_results.to_csv("/Users/runminghuang/Desktop/capstone/Data/ridge_regression_by_SECTOR_20250303.csv", index=False)
